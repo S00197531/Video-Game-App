4993b420fe29985330fe8ac071d993b27b550215
Adam Saul <S00197531@mail.itsligo.ie>
2022-08-05 21:41:52 +0100

    connected to mongodb created routes and users

--- server/app.js
+++ server/app.js
@@ -4,11 +4,35 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database')
+
+mongoose.connect(config.database)
+
+mongoose.connection.on('connected', () => {
+    console.log('Conected to database '+config.database)
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log('error to database '+err)
+})
 
 const app = express();
 
+const users = require('./routes/users');
+
 const port = 3000;
 
+app.use(cors());
+
+//Set Static Folder
+//app.use(express.static(path.join(__dirname, 'src')))
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+//Index Route
 app.get('/', (req, res) => {
     res.send('Invalid Endpoint')
 })
---
+++ server/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://localhost:27017/videogameApp',
+    secret: 'yoursecret'
+}
