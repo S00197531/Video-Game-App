db84663de719ccb3246107b7458a43c8beedfc55
Adam Saul <S00197531@mail.itsligo.ie>
2022-08-06 22:41:18 +0100

    register api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const User = require('../models/user')
 const mongoose = require('mongoose')
 const db = "localhost:27017"
 
@@ -14,7 +14,19 @@ mongoose.connect(db, err => {
 
 router.get('/', (req, res) => {
     res.send('From API Route');
-});
+})
+
+router.post('/register', (req,res) => {
+    let userData = req.body
+    let user = new User(userData)
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.status(200).send(registeredUser)
+        }
+    })
+})
 
 
 module.exports = router;
