0bf81083a02ad9a098ed1b8e2149f8b00759743d
Adam <S00197531@mail.itsligo.ie>
2022-08-11 10:33:19 +0100

    JWT now being generated in back-end

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user')
 const mongoose = require('mongoose')
 const db = "mongodb+srv://dbUser:dbpass@eventsdb.bchvp.mongodb.net/eventsdb"
+const jwt = require('jsonwebtoken')
 
 mongoose.connect(db, err => {
     if (err) {
@@ -23,7 +24,9 @@ router.post('/register', (req,res) => {
         if (error) {
             console.log(error)
         } else {
-            res.status(200).send(registeredUser)
+            let payload = { subject: registeredUser._id }
+            let token = jwt.sign(payload, 'secretKey')
+            res.status(200).send({token})
         }
     })
 })
@@ -41,7 +44,9 @@ router.post('/login', (req,res) => {
             if (user.password !== userData.password) {
                 res.status(401).send('invalid password')
             } else {
-                res.status(200).send(user)
+                let payload = {subject: user._id}
+                let token = jwt.sign(payload, 'secretKey')
+                res.status(200).send({token})
             }
         }
     })
